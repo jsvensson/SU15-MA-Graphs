3eaaef4fd69698e58ad61901efca3bdfcde0e452
Johan Svensson <johan@atomicplayboy.net>
2016-02-24 12:35:46 +0100

    Initial commit

---
+++ Graphs/Graph.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+	public class Graph
+	{
+		private bool[,] adjMatr;
+
+		public struct DoDFSReturn
+		{
+			bool hasLoop;
+			bool isConnected;
+		}
+
+		private uint nrNodes;
+
+		public uint NrNodes
+		{
+			get
+			{
+				return this.nrNodes;
+			}
+			private set
+			{
+				if( value == 0 )
+				{
+					throw new ArgumentOutOfRangeException(
+						"The graph must have atleast one node.",
+						"NrNodes"
+					);
+				}
+
+				this.nrNodes = value;
+			}
+		}
+
+		public Graph( uint nrNodes )
+		{
+			this.NrNodes = nrNodes;
+			this.adjMatr = new bool[nrNodes,nrNodes];
+		}
+
+		public List<uint> GetNeighbours( uint node )
+		{
+			if (node >= this.NrNodes)
+			{
+				throw new ArgumentOutOfRangeException(
+					$"This graph has {this.NrNodes} nodes and thus " +
+					$"no node named '{node}'"
+				);
+			}
+
+			List<uint> neighbours = new List<uint>();
+
+			for( uint i = 0; i < this.adjMatr.GetLength(1); i++ )
+			{
+				if( this.adjMatr[node,i] == true )
+				{
+					neighbours.Add(i);
+				}
+			}
+
+			return neighbours;
+		}
+
+		public void AddEdges( uint[,] edges )
+		{
+			if( edges.GetLength(1) != 2 )
+			{
+				throw new ArgumentException(
+					"The second dimension of arg edges must be 2.",
+					"edges"
+				);
+			}
+
+			foreach( uint node in edges )
+			{
+				if( node >= this.NrNodes )
+				{
+					throw new ArgumentOutOfRangeException(
+						$"This graph has {this.NrNodes} nodes and thus "+
+						$"no node named '{node}'",
+						"edges"
+					);
+				}
+			}
+
+			for( uint i = 0; i < edges.GetLength( 0 ); i++ )
+			{
+				this.adjMatr[edges[i, 0], edges[i, 1]] = true;
+				this.adjMatr[edges[i, 1], edges[i, 0]] = true;
+			}
+		}
+
+		public bool DoDFS( uint startNode = 0 )
+		{
+			if (startNode>= this.NrNodes)
+			{
+				throw new ArgumentOutOfRangeException(
+					$"This graph has {this.NrNodes} nodes and thus " +
+					$"no node named '{startNode}'",
+					"startNode"
+				);
+			}
+
+			return true;
+		}
+
+		public static void Main()
+		{
+
+		}
+	}
+}
---
+++ GraphsTests/GraphTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Tests
+{
+	[TestClass()]
+	public class GraphTests
+	{
+		[TestMethod()]
+		public void GetNeighbours__4_Nodes__2_Neighbours()
+		{
+			Graph graph = new Graph(4);
+
+			graph.AddEdges(
+				new uint[6,2] {{1,1}, {3,0}, {2,3}, {2,1}, {0,1}, {2,0}}
+			);
+
+			List<uint> neighbours = graph.GetNeighbours(3);
+
+			Assert.IsTrue(
+				neighbours.Count == 2 &&
+				neighbours.Contains( 0 ) &&
+				neighbours.Contains( 2 )
+			);
+		}
+
+		[TestMethod()]
+		public void GetNeighbours__1024_Nodes__No_Neighbours()
+		{
+			Graph graph = new Graph(1024);
+
+			graph.AddEdges(
+				new uint[3, 2] { { 732, 254 }, { 958, 121 }, { 24, 654 } }
+			);
+
+			List<uint> neighbours = graph.GetNeighbours(65);
+
+			Assert.IsTrue( neighbours.Count == 0 );
+		}
+
+		[TestMethod()]
+		public void GetNeighbours__128_Nodes__Node_Is_Its_Own_Neighbour()
+		{
+			Graph graph = new Graph(128);
+
+			graph.AddEdges(
+				new uint[1, 2] { { 103, 103 } }
+			);
+
+			List<uint> neighbours = graph.GetNeighbours( 103 );
+
+			Assert.IsTrue( neighbours.Count == 1 && neighbours[0] == 103 );
+		}
+	}
+}
